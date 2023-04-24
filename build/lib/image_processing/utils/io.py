1af10b2c7bf58e1e25e1be9a59519576d6386b62
Mauricio Oliveira <mauricio.oliveira8583@gmail.com>
2023-04-24 15:31:45 -0300

    Final commit

---
+++ build/lib/image_processing/utils/io.py
@@ -0,0 +1,9 @@
+from skimage.io import imread
+from skimage.io import imsave
+
+def read_image(path, is_gray = False):
+    image = imread(path, as_gray = is_gray)
+    return image
+
+def save_image(image, path):
+    imsave(path, image)
